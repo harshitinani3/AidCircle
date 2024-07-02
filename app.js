f58ae48f1e846061174a26b2bfd76b97352df027
harshitinani3 <harshit.inani.mat21@itbhu.ac.in>
2024-07-03 02:14:59 +0530

    add

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb+srv://madhavbansalmat21:test123@cluster0.cppnnr2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
