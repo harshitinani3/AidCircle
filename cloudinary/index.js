3cf45825478f279544c736f7aab91fb485d8d406
Madhav <madhav.bansal.mat21@itbhu.ac.in>
2024-07-02 23:13:53 +0530

    styling

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'AidCircle',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
