37992b9347c5cc928b3fddb1aed82ca51a4e6d5e
Madhav <madhav.bansal.mat21@itbhu.ac.in>
2024-07-03 00:02:52 +0530

    map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [77.1025, 28.7041],
+    zoom: 4
 });
 
 map.addControl(new mapboxgl.NavigationControl());
