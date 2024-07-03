13427eed39c8d99aa68c2716836c68de61106541
harshitinani3 <harshit.inani.mat21@itbhu.ac.in>
2024-07-03 10:32:42 +0530

    finally

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/db';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
